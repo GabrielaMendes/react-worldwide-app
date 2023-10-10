b3166a3183ff97f4a302958e75eee7ee23f22f96
Gabriela <gdemelo.mendes@gmail.com>
2023-10-10 16:52:25 -0300

    Added list of visited cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
-import Pricing from "./pages/Pricing"
-import Product from "./pages/Product"
-import Login from "./pages/Login"
-import AppLayout from "./pages/AppLayout"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch {
+				alert("There was an error loading data...");
+			} finally {
+				setIsLoading(false);
+			}
+		}
+    fetchCities()
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -15,8 +38,8 @@ function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<p>List of cities</p>} />
-					<Route path="cities" element={<p>List of cities</p>} />
+					<Route index element={<Navigate to="/app/cities" />} />
+					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
 					<Route path="countries" element={<p>List of countries</p>} />
 					<Route path="form" element={<p>Form</p>} />
 				</Route>
