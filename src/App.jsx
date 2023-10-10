101438da762e4ff9e9b34991a26a8ed221516260
Gabriela <gdemelo.mendes@gmail.com>
2023-10-10 09:52:45 -0300

    Added login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
-import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
+import Product from "./pages/Product"
+import Login from "./pages/Login"
 import AppLayout from "./pages/AppLayout"
 import PageNotFound from "./pages/PageNotFound";
 
@@ -10,8 +11,9 @@ function App() {
 		<BrowserRouter>
 			<Routes>
         <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
