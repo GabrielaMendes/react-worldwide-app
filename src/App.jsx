6c1fa99b3c6f216bbe4aa0ff72103cf00775bde8
Gabriela <gdemelo.mendes@gmail.com>
2023-10-11 10:01:38 -0300

    Fixed navigate history

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<Navigate to="cities" />} />
+					<Route index element={<Navigate replace to="cities" />} />
 					<Route
 						path="cities"
 						element={<CityList cities={cities} isLoading={isLoading} />}
