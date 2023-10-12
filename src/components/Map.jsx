4a3b3ecacbe55ffd47d88d699587240ecaf66669
Gabriela <gdemelo.mendes@gmail.com>
2023-10-12 18:16:24 -0300

    Added marker and popup to each city in list

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,31 +1,46 @@
-import { useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import styles from './Map.module.css'
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./Map.module.css";
 
 function Map() {
-  const navigate = useNavigate()
-  const [mapPosition, setMapPosition] = useState([40, 0])
+	const navigate = useNavigate();
 
-  const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get("lat")
-  const lng = searchParams.get("lng")
+	const { cities } = useCities();
 
-  return (
+	const [mapPosition, setMapPosition] = useState([40, 0]);
+
+	const [searchParams, setSearchParams] = useSearchParams();
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
+
+	return (
 		<div className={styles.mapContainer} onClick={() => navigate("form")}>
-			<MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+			<MapContainer
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
+				className={styles.map}
+			>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
 				/>
-				<Marker position={mapPosition}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+				{cities.map((city) => (
+					<Marker
+						position={[city.position.lat, city.position.lng]}
+						key={city.id}
+					>
+						<Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+						</Popup>
+					</Marker>
+				))}
 			</MapContainer>
 		</div>
 	);
 }
 
-export default Map
+export default Map;
